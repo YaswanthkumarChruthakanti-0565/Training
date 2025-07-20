4ac250d5d8a43e4b572516928727b7d1f8fe352c
Yashwanth <117254398+YaswanthkumarChruthakanti-0565@users.noreply.github.com>
2025-07-20 12:25:01 +0530

    Add files via upload

---
+++ Yash/mysite/welcome/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from.import views
+urlpatterns=[
+    path('wel/',views.wel,name='wel'),
+]
---
+++ Yash/mysite/welcome/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def wel(request):
+    return HttpResponse("Welcome to my website")
+# Create your views here.
